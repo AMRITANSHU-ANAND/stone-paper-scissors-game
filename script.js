bc35a823ce3b03f666dbaf6f82aa5624fe0722b9
Amritanshu Anand <amritanshusingh1669@gmail.com>
2026-05-03 08:30:18 +0530

    feat: implement game logic with user choice, computer random selection, and draw condition

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ let computer = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const getComputerChoice = () => {
+    const optionsChoice =["rock","paper","scissors"]
+    const randIndex = Math.floor(Math.random()*3);
+    return optionsChoice[randIndex];
+};
+
+const draw =()=>{
+    console.log("It,s a draw!");
+}
+const playGame =(userChoice)=>{
+    console.log("User chose:", userChoice);
+    // generate computer choice
+    const computerChoice = getComputerChoice();
+    console.log("Computer chose:", computerChoice);
+    //draw condition
+    if (userChoice === computerChoice){
+        draw();
+    };
+
+};
+
 choices.forEach((choice)=>{
     choice.addEventListener("click",()=>{
         const userChoice = choice.getAttribute("id");
         console.log(userChoice);
+        playGame(userChoice);
     });
 });
